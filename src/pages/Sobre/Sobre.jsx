ca6a2f8e7216c0fad58a2fddfa14328b55fdb38f
Thayanne Andrade <thayannesandrade@gmail.com>
2022-07-29 01:33:01 -0300

    refactor page Sobre

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,9 +1,22 @@
-import Title from "../../components/Title/Title"
+import Title from "../../components/Title/Title";
+import Text from "../../components/Text/Text";
+import { Container, ContainerText } from "../../styles/containers";
+
+import CONTACTIMG from "../../assets/contact-me.svg"
+import { ContainerImage } from "./sobre.style";
 
 function Sobre() {
     return (
         <>
-        <Title title="Sobre"/>
+        <Container>
+            <Title title="Sobre"/>
+            <ContainerText>
+                <Text text="Lorem ipsum dolor sit amet. Qui possimus accusantium aut molestiae eius quo numquam provident qui voluptas similique. Ea repudiandae rerum aut voluptatem Quis aut obcaecati recusandae in itaque error. Ut earum nemo sit odio eligendi ab facilis dignissimos!"/>
+            </ContainerText>
+            <ContainerImage>
+                    <img src={CONTACTIMG} alt="svg com uma mulher negra, de cabelos curtos e pretos, de blusa branca, calça preta e bota preta, segurando com a mão direita uma caixa de diálogo e na mão esquerda um balão de confirmação." />
+            </ContainerImage>
+        </Container>
         </>
     )
 }
---
+++ src/pages/Sobre/sobre.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ContainerImage = styled.div`
+    width: 100%;
+    height: 350px;
+    overflow: hidden;
+    margin: 0 auto;
+    padding: 12px 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img{
+        max-width: 100%;
+        max-height: 100%;
+    }
+`
