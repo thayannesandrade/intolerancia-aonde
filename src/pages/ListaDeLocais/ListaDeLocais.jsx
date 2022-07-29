a244e88b76d0fcbf1bd3a0d6f4c343d5ad0e9360
Thayanne Andrade <thayannesandrade@gmail.com>
2022-07-29 01:33:36 -0300

    refactor Lista de Locais page

--- src/pages/ListaDeLocais/ListaDeLocais.jsx
+++ src/pages/ListaDeLocais/ListaDeLocais.jsx
@@ -1,9 +1,40 @@
-import Title from "../../components/Title/Title"
+import Title from "../../components/Title/Title";
+import Card from "../../components/Card/Card";
+import Text from "../../components/Text/Text";
+
+import { ContainerCard } from "./listaDeLocais.style";
+import { Container, ContainerText } from "../../styles/containers";
+import { useEffect, useState } from "react";
 
 function ListaDeLocais() {
+    const [local, setLocal] = useState([]);
+    const url = `http://localhost:3000/locais`;
+
+    useEffect(() => {
+        async function loadLocal() {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            setLocal(data);
+        }
+
+        loadLocal();
+    }, [])
+
     return (
         <>
-        <Title title="Lista de Locais"/>
+        <Container>
+            <Title title="Lista de Locais"/>
+            <ContainerText>
+                <Text text="Lorem ipsum dolor sit amet. Qui possimus accusantium aut molestiae eius quo numquam provident qui voluptas similique. Ea repudiandae rerum aut voluptatem Quis aut obcaecati recusandae in itaque error. Ut earum nemo sit odio eligendi ab facilis dignissimos!" />
+            </ContainerText>
+
+            <ContainerCard>
+                {local.map(
+                    (local, index) => <Card key={index} local={local} /> 
+                )}
+            </ContainerCard>
+        </Container>    
         </>
     )
 }
---
+++ src/pages/ListaDeLocais/listaDeLocais.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContainerCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    align-items: center;
+    justify-content: center;
+    padding: 12px 12px;
+`
+   
